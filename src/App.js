f48c2e6cea641d8f098a5cf908fbfd2adcbba286
Otmane Aatik <aaotmane@gmail.com>
2021-10-05 11:56:15 -0400

    task delete

--- src/App.js
+++ src/App.js
@@ -27,10 +27,20 @@ function App() {
 
     }
 ])
+
+//Delete Task 
+const deleteTask = (id) => {
+  setTasks(tasks.filter(task => task.id !== id))
+}
+
   return (
     <div className="container">
     <Header />
-    <Tasks tasks={tasks}/>
+    {
+    tasks.length > 0 ? (
+    <Tasks tasks={tasks} onDelete={deleteTask}/>
+    ): ("NO TASKS TO SHOW")
+    }
     </div>
   );
 }
--- src/components/Task.js
+++ src/components/Task.js
@@ -1,9 +1,13 @@
-import React from 'react'
+import { FaTimes } from "react-icons/fa";
 
-function Task( { task }) {
+function Task( { task, onDelete }) {
     return (
         <div className="task">
-            <h3>{task.text}</h3>
+            <h3>
+                {task.text} 
+                <FaTimes style={{ color: "red", pointer: "cursor"}}
+                onClick={() => onDelete(task.id)} />
+            </h3>
             <p>{task.day}</p>
         </div>
     )
